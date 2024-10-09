e79e167cd61f8b5a0903dccb5634a14ecc91cd10
Rashena Hanif <rashenahanif@MacBookAir.lan>
2024-10-08 21:03:52 -0400

    Add files

---
+++ coursework/block07/code.js
@@ -0,0 +1,16 @@
+/**
+ * 1) You have received this message because you have been chosen to open an important vault. "You have recieved this message because you have been chosen to open an important vault"
+ * 2) Declare and assign 3 variables, one for each number in the combination.
+ * 3) Code1 will be useing +
+ * 4) Code2 will be using -
+ * 5) Code3 will be using *
+ * 6) Display the string and the codes to the user
+ */
+
+const userMessage = "You have received this message because you have been chosen to open an important vault."
+
+let Code1 = 1 + 1
+let Code2 = 10 - 5
+let Code3 = 7 * 3
+
+alert(userMessage + "\n" + Code1 )
